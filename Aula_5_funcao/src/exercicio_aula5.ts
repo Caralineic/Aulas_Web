f57ac8728ef2bc5c060eb73f7938c05d1cf8a8ab
Caralineic <carol2711campos@gmail.com>
2026-03-05 22:19:36 -0300

    EXERCICIOS.2_3_4_5

--- Aula_5_funcao/src/exercicio_aula5.ts
+++ Aula_5_funcao/src/exercicio_aula5.ts
@@ -2,7 +2,51 @@
 function maiuscula(m: string): string{
    return m.toUpperCase();
 }
-console.log(maiuscula("Ola mundo"));
+console.log(maiuscula("Olá mundo"));
 
+//lambda
 const maiuscula_lambda = (m: string):string => m.toUpperCase(); 
 console.log(maiuscula_lambda("Olá mundo!!"));
+
+//EXERCICIO 2
+function elevar(a: number, b:number): number{
+   return (a ** b);
+}
+
+console.log(elevar(5,2));
+
+//EXERICIO 3 
+
+
+//EXERCICIO 4
+type Funcionario = {
+    nome : string ;
+    cargo : string ;
+   };
+   
+   const funcionarios : Funcionario [] = [
+   { nome : " Marcos ", cargo : " Desenvolvedor " } ,
+   { nome : " Fernanda ", cargo : " Gerente " } ,
+   { nome : " Carlos ", cargo : " Desenvolvedor " } ,
+   { nome : " Joana ", cargo : " Analista " }
+    ];
+
+   let cargoDesejado = funcionarios.filter(Funcionario => Funcionario.cargo === " Desenvolvedor ");
+   console.log(cargoDesejado);
+
+
+//EXERCICIO 5
+type Aluno = {
+   nome : string ;
+   nota : number ;
+   };
+   
+   const alunos : Aluno [] = [
+   { nome : " Alice ", nota : 8 } ,
+   { nome : " Bruno ", nota : 5 } ,
+   { nome : " Carla ", nota : 7 } ,
+   { nome : " Daniel ", nota : 6 }
+   ];
+   
+   let Aprovados = alunos.filter(Aluno => Aluno.nota >= 7);
+   console.log(Aprovados);
