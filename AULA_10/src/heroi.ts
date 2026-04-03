67386ffd2d1384eda21c359dc2aa6df84c39897c
Caralineic <carol2711campos@gmail.com>
2026-04-02 21:42:31 -0300

    exercicio_10

--- AULA_10/src/heroi.ts
+++ AULA_10/src/heroi.ts
@@ -4,14 +4,36 @@ export class heroi{
     nome: string;
     categoria: Categoria;
     ataque: number;
-    recurso: number; 
+    custo: number;
+    recurso: number;
+    Tiporecurso: string; 
 
 
-    constructor(nome: string, categoria: Categoria, ataque: number, recurso:number){
+    constructor(nome: string, categoria: Categoria, ataque: number, custo: number){
         this.nome = nome;
         this.categoria = categoria;
         this.ataque = ataque;
-        this.recurso = recurso;
+        this.custo = custo
+
+        if(this.categoria === "Espadachim"){
+            this.recurso = 50;
+            this.Tiporecurso = "Stamina"
+        }else if (this.categoria === "Mago"){
+            this.recurso = 40;
+            this.Tiporecurso = "Mana"
+        }else{
+            this.recurso = 10;
+            this.Tiporecurso = "Flechas"
+        }
+    }
+
+    atacar():void{
+        if(this.recurso >= this.custo){
+            this.recurso -= this.custo;
+            console.log (`Ataque de ${this.nome} bem sucedido , restando ${this.recurso}`);
+        }else{
+            console.log(`Ataque de ${this.nome} falhou por falta de recurso`)
+        }
     }
 
 }
---
+++ AULA_10/src/index.ts
@@ -0,0 +1,5 @@
+import {heroi} from "./heroi"
+
+const Categoria1 = new heroi("Joana", "Espadachim", 20, 40);
+
+Categoria1.atacar()
