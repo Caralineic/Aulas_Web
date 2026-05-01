bbbdba13989ed4dc383190e844b0a7f61a051152
Caralineico <carol2711campos@gmail.com>
2026-05-01 12:06:47 -0300

    final_aula_17

--- projeto-drones/src/models/Voo.ts
+++ projeto-drones/src/models/Voo.ts
@@ -6,10 +6,10 @@ import {Pacote} from "./Pacote";
 export class Voo{
     id: string;
     data_envio: string;
-    pacotes: Pacote;
+    pacotes: Pacote[];
     descricao: descricao;
 
-    constructor(id: string, data_envio: string, pacotes: Pacote, descricao: descricao){
+    constructor(id: string, data_envio: string, pacotes: Pacote[], descricao: descricao){
         this.id = id;
         this.data_envio = data_envio;
         this.pacotes = pacotes;
--- projeto-drones/src/models/main.ts
+++ projeto-drones/src/models/main.ts
@@ -1,14 +1,28 @@
 import {Voo} from "./Voo"
+import {Pacote} from "./Pacote"
+import {descricao} from "./descricao"
 
 const jsonBruto ='{" id_voo ": "DRN -7742" , " data_envio ": "2026 -04 -28" , " detalhes ": { "altitude_max ": 120 , "sensores ": [" GPS " , " Termometro " , " Lidar "] }, " pacotes ": [ {"peso ": 1.2 , " descricao ": " Medicamentos "} , {" peso ": 0.8 , " descricao ": " Suprimentos "}] }';
 
  class Main {
     static executar () {
- const dadosSimulados = JSON.parse ( jsonBruto ) ;
- console . log (" --- Iniciando Sistema de Logistica ---") ;
+ const dadosSimulados = JSON.parse(jsonBruto);
+ console.log(" --- Iniciando Sistema de Logistica ---");
 
- /*const meuVoo = new Voo( dadosSimulados );
- console.log ("Total a transportar : " + meuVoo . getPesoTotal () + "kg ");*/
+ const meuVoo = new Voo(
+   dadosSimulados[" id_voo "].trim(),
+   dadosSimulados[" data_envio "].trim(),
+   dadosSimulados[" pacotes "].map((pacote: any) => new Pacote(
+     Number(pacote["peso "]),
+     pacote[" descricao "].trim()
+   )),
+   new descricao(
+     Number(dadosSimulados[" detalhes "]["altitude_max "]),
+     dadosSimulados[" detalhes "]["sensores "].map((sensor: string) => sensor.trim())
+   )
+ );
+
+ console.log("Voo instanciado:", meuVoo);
  } }
 
 Main.executar();
